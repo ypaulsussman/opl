7b2d228f30aa73f7cf2625ecb709ac42c7a9c54c
Y Paul Sussman <ypaulsussman@gmail.com>
2019-10-02 21:18:38 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+authors = CSV.read(Rails.root.join('db', 'opl_authors_seed.csv'), headers: true)
+authors.each do |row|
+  author = row['Author']
+  Author.create!(name: author)
+  puts "Added #{author}..."
+end
+
+quotes = CSV.read(Rails.root.join('db', 'opl_quotes_seed.csv'), headers: true)
+quotes.each do |row|
+  author = row['Author']
+  a = Author.find_by(name: author)
+  Quote.create!(passage: row['Quote'], author: a)
+  puts "Added quote by #{author}..."
+end
