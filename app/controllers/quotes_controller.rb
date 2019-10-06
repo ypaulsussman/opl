63bcb1bc997f4914bb67c28f78965006e9dca9b9
Y Paul Sussman <ypaulsussman@gmail.com>
2019-10-06 00:20:18 -0500

    add author-select dropdown to form-partial

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -77,6 +77,6 @@ class QuotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def quote_params
-      params.fetch(:quote, {}).permit(:passage)
+      params.fetch(:quote, {}).permit(:passage, :author_id)
     end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -2,14 +2,9 @@
 
 class Author < ApplicationRecord
   has_many :quotes, dependent: :destroy
-  scope :by_surname, lambda {
-    all
-      .map { |a| { name: a.name, sort_name: a.name.split[-1].downcase, id: a.id } }
-      .sort_by { |a| a[:sort_name]}
-      # .map { |a| { name: a, id: a[:id] } }
-  }
+  scope :by_surname, lambda { all.sort_by(&:sortable_name) }
 
-  def stylized_name
-    "yep, it's #{name}"
+  def sortable_name
+    name.split[-1].downcase
   end
 end
