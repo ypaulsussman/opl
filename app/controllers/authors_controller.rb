eab6dec71526a577144f969ad869aa78e6cb4366
Y Paul Sussman <ypaulsussman@gmail.com>
2019-10-06 16:21:50 -0500

    add (mostly) views/endpoints for authors

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,72 @@
+class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update, :destroy]
+
+  # GET /authors
+  # GET /authors.json
+  def index
+    @authors = Author.order(quotes_count: :desc).page params[:page]
+  end
+
+  # GET /authors/{uuid}
+  # GET /authors/{uuid}.json
+  def show
+  end
+
+  # GET /authors/new
+  def new
+    @author = Author.new
+  end
+
+  # GET /authors/{uuid}/edit
+  def edit
+  end
+
+  # POST /authors
+  # POST /authors.json
+  def create
+    @author = Author.new(author_params)
+
+    respond_to do |format|
+      if @author.save
+        format.html { redirect_to @author, notice: 'Author was successfully created.' }
+        format.json { render :show, status: :created, location: @author }
+      else
+        format.html { render :new }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /authors/{uuid}
+  # PATCH/PUT /authors/{uuid}.json
+  def update
+    respond_to do |format|
+      if @author.update(author_params)
+        format.html { redirect_to @author, notice: 'Author was successfully updated.' }
+        format.json { render :show, status: :ok, location: @author }
+      else
+        format.html { render :edit }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /authors/{uuid}
+  # DELETE /authors/{uuid}.json
+  def destroy
+    @author.destroy
+    respond_to do |format|
+      format.html { redirect_to authors_url, notice: 'Author was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  def author_params
+    params.fetch(:author, {}).permit(:name, quote_attributes: [:passage, :id, :_destroy])
+  end
+end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  default_scope { order(created_at: :asc) }
+  # @TODO: originally the below was included w/ the above
+  # b/c uuid's not a great default sort -- however,
+  # it's already caused more problems than it appears to solve... delete?
+  # default_scope { order(created_at: :asc) }
 end
