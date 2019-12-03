a0b86a6750164b0a167bff7a165ef38266221dd9
Y Paul Sussman <ypaulsussman@gmail.com>
2019-12-02 19:35:33 -0600

    - fix off-by-one error in send-at-date rake task - temporary cosmetic qotd-email changes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@example.com'
+  default from: 'ypaulsussman@gmail.com'
   layout 'mailer'
 end
