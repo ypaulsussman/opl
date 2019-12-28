6f4993f8e4c4680462e4b8f2fe3ee28db1b2304c
Y Paul Sussman <ypaulsussman@gmail.com>
2019-12-28 15:27:02 -0600

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
   root 'quotes#index'
+
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :quotes
-  resources :authors
-  resources :users
-
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+
+  resources :users, :authors, :quotes
 end
