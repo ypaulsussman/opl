ffdcb50851ef75bc786055b19d42b72affd01d44
Y Paul Sussman <ypaulsussman@gmail.com>
2020-01-03 18:12:04 -0600

    make job test sequence-indifferent

--- test/jobs/send_qotd_email_job_test.rb
+++ test/jobs/send_qotd_email_job_test.rb
@@ -13,17 +13,18 @@ class SendQotdEmailJobTest < ActiveJob::TestCase
   end
 
   test 'does nothing if no quote is available for today' do
-    @our_qotd.update!(next_send_at: Date.tomorrow)
-    SendQotdEmailJob.perform_now
-
-    assert_equal 0, ActionMailer::Base.deliveries.count
+    assert_no_difference('ActionMailer::Base.deliveries.count') do
+      @our_qotd.update!(next_send_at: Date.tomorrow)
+      SendQotdEmailJob.perform_now
+    end
   end
 
   test "sends today's quote solely to subscribed users" do
-    @our_qotd.update!(next_send_at: Date.today)
-    SendQotdEmailJob.perform_now
+    assert_difference('ActionMailer::Base.deliveries.count', 1) do
+      @our_qotd.update!(next_send_at: Date.today)
+      SendQotdEmailJob.perform_now
+    end
 
-    assert_equal 1, ActionMailer::Base.deliveries.count
     mail = ActionMailer::Base.deliveries.first
     assert_equal @subbed_user.email, mail.to.first
     assert_match @our_qotd.passage, mail.body.encoded
