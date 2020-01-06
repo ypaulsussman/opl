00e185ca13ac4b77875f80fe21c8046a0258ef46
Y Paul Sussman <ypaulsussman@gmail.com>
2020-01-05 19:30:03 -0600

    - Dependency treadmill - Fix misscoped Rubocop

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -9,6 +9,9 @@ class Quote < ApplicationRecord
 
   private
 
+  # TODO: when Rails 6.1 drops, replace this with a check against 
+  # thequotes_count of author_previously_was, after_update, a la
+  # https://blog.bigbinary.com/2019/12/03/rails-6-1-adds-_previously_was-attribute-methods.html
   def check_for_orphaned_author
     prior_author = Author.find(author_id_was)
     @orphaned_author = prior_author unless prior_author.quotes_count > 1
