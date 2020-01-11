9be41f2694d0ba777493df1d933e583ddd6dc4e6
Y Paul Sussman <ypaulsussman@gmail.com>
2020-01-11 11:58:34 -0600

    - Refactor orphan-author removal to use`previous_changes`

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,17 +3,16 @@
 class Quote < ApplicationRecord
   belongs_to :author, counter_cache: true
   validates :passage, presence: true, uniqueness: true
-  before_update :check_for_orphaned_author, if: -> { author_id_changed? }
-  after_update -> { @orphaned_author.destroy }, if: -> { @orphaned_author.present? }
+  after_update :remove_orphaned_author
   after_destroy -> { author.destroy }, if: -> { author.quotes_count.zero? }
 
   private
 
-  # TODO: when Rails 6.1 drops, replace this with a check against 
-  # thequotes_count of author_previously_was, after_update, a la
-  # https://blog.bigbinary.com/2019/12/03/rails-6-1-adds-_previously_was-attribute-methods.html
-  def check_for_orphaned_author
-    prior_author = Author.find(author_id_was)
-    @orphaned_author = prior_author unless prior_author.quotes_count > 1
+  def remove_orphaned_author
+    return unless previous_changes[:author_id].present?
+
+    prior_author_id = previous_changes[:author_id][0]
+    prior_author = Author.find(prior_author_id)
+    prior_author.destroy unless prior_author.quotes_count.positive?
   end
 end
