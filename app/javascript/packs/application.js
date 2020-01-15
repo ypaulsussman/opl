61e132b66d184b327f8a7dd9828cdd9caf161952
Y Paul Sussman <ypaulsussman@gmail.com>
2020-01-14 19:53:02 -0600

    - Move uswds css-require to webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("turbolinks").start()
 // require("@rails/activestorage").start()
 // require("channels")
 
+require('uswds/dist/css/uswds')
 require("uswds").start();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
