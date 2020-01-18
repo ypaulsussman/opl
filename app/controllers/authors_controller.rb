f97a4fbf6514d6b9ce48d26223c91d6a13fdf337
Y Paul Sussman <ypaulsussman@gmail.com>
2020-01-18 12:23:44 -0600

    - Extract (browse/new) quotes/authors header-links to partial

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -17,6 +17,8 @@ class AuthorsController < ApplicationController
       .page(params[:page])
       .per(36)
 
+    # Yes, this is gross: its function is to turn the AR Relation of authors into a nested array,
+    # with the inner arrays each containing three (or, in the final case, 1-3) author-records.
     @authors_in_threes = @authors.to_a.each_with_object([]).with_index do |(author, ait_agg), i|
       if (i % 3).zero?
         ait_agg.push([author])
@@ -24,21 +26,6 @@ class AuthorsController < ApplicationController
         ait_agg[-1].push(author)
       end
     end
-
-    # @authors_in_threes = @authors.to_a.each_with_index.reduce([]) do |ait, (a, i)|
-    #   if (i % 3).zero?
-    #     ait.push([a])
-    #     puts "ya ait! #{ait}, and index: #{i}"
-    #   else
-    #     puts "we adding to #{ait[-1]}"
-    #     ait[-1].push(a)
-    #   end
-    #   ait
-    # end
-
-    # @authors1 = @authors.to_a.to(11)
-    # @authors2 = @authors.to_a.from(12).to(23)
-    # @authors3 = @authors.to_a.from(24).to(35)
   end
 
   # GET /authors/{slug}
