9ec9f39991d9b98718457ddd28cd0d4992cad938
Y Paul Sussman <ypaulsussman@gmail.com>
2020-01-20 22:50:55 -0600

    - remove now-unused js file - revert search-button styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,19 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
+require("@rails/ujs").start();
+require("turbolinks").start();
 
-require('uswds/dist/css/uswds')
-require("uswds")
+require("uswds/dist/css/uswds");
+require("uswds");
 
-import('./main.scss')
-import('./quote_search.js')
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import("./main.scss");
