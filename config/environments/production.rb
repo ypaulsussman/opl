b154ec6bbcd42b64b34163cbd1571301c9ffe35b
Y Paul Sussman <ypaulsussman@gmail.com>
2020-10-21 19:23:28 -0500

    mailgun switch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,13 +29,12 @@ Rails.application.configure do
   host = 'https://dry-thicket-30439.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'itsopl.heroku.com',
     authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
   }
 
   # Don't let anyone else join the site, currently, to avoid going over SendGrid's free-tier limit.
